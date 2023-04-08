0f393cd2e6c9f1354c0aef426bad92a769d103a8
akash5k <akashparida185@gmai.comk>
2023-04-09 01:07:35 +0530

    added pages

--- static/script.js
+++ static/script.js
@@ -1,3 +1,12 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
+});
