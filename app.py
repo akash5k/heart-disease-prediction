a94e40499f0f672bc3dedf05b128a04ec68993d9
akash5k <akashparida185@gmai.comk>
2023-05-29 11:04:54 +0530

    deploy test

--- app.py
+++ app.py
@@ -54,5 +54,6 @@ def predict():
 #If we import this file (module) to another file then __name__ == app (which is the name of this python file).
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run()
+    # app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
     # run on devices with same network with address 0.0.0.0 and port 5000
